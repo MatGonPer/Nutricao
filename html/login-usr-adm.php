d8e31eff5b830a6e4265ed07e121672ee6ced25c
Matheus Gonçalves Pereira <matheushtht@gmail.com>
2025-04-11 18:22:13 -0300

    Adicionando HTML e CSS da página Login: usuário/administrador. Ainda incompleto, precisa ser terminado.

--- html/login-usr-adm.php
+++ html/login-usr-adm.php
@@ -4,39 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acesse sua conta</title>
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/login-usr-adm.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Yrsa:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/login-usr-adm.css">
 </head>
-
 <body>
-    <main class="container">
+    <main>
         <form action="">
-            <h1>Acesse sua conta</h1>
-            <div class="input-box">
+        <section class="container">    
+            <h2>Acesse sua conta</h1>
+            <section class="input-box">
                 <label for="email">Email:</label>
+                <br>
                 <input placeholder="Digite o seu email" type="email">
-                <img src="/assets/icons/login-register/user-icon.svg" alt="Ícone de usuário" width="22" height="22">
-            </div>
-            <div class="input-box">
+                <img src="../assets/icons/login-register/user-icon.svg" alt="Ícone de usuário" width="22" height="22">
+            </section>
+            <section class="input-box">
                 <label for="password">Senha:</label>
+                <br>
                 <input placeholder="Digite a sua senha" type="password">
-                <img src="/assets/icons/login-register/password-icon.svg" alt="Ícone de usuário" width="22" height="22">
-            </div>
-            <div class="remember-forgot">
+                <img src="../assets/icons/login-register/password-icon.svg" alt="Ícone de usuário" width="22" height="22">
+            </section>
+            <section class="remember-forgot">
                 <label for="checkbox">
                     <input type="checkbox">
                     Lembre-me
                 </label>
                 <a href="">Esqueci minha senha</a>
-            </div>
+            </section>
                 <button type="submit">Acessar conta</button>
-            <div class="register-link">
+            <section class="register-link">
                 <p>Não tem uma conta? <a href="">Cadastre-se</a></p>
-            </div>
-        </form>
+            </section>
+        </section>    
+        </form>  
     </main>
 </body>
 </html>
