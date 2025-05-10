7b7a8f58c58ed890a349238b4605b1d37be628ea
Arthur <75622979+800Arthur008@users.noreply.github.com>
2025-05-10 20:04:21 -0300

    Adicionada a pagina dashboard-user

---
+++ html/dashboard-usuario.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desempenho do Usuário</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+   <div class="container">
+        <section class="left">
+            <aside>
+                <nav>
+                    <ul>
+                        <li><a href="#">Início</a></li>
+                        <li><a href="#">Usuário</a></li>
+                        <li><a href="#">Desempenho</a></li>
+                        <li><a href="#">Treino</a></li>
+                        <li><a href="#">Consultas</a></li>
+                        <li><a href="#">Catálogo</a></li>
+                        <li><a href="#">Suporte</a></li>
+                        <li><a href="#">Configurações</a></li>
+                        <li><a href="#">Sair</a></li>
+                    </ul>
+                </nav>
+            </aside>
+        </section>
+        <section class="right"> 
+
+        </section>
+</body>
+</html>
