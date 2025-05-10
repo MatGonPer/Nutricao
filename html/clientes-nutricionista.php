632c0f7d68e2d5d9a6629fb1b82fdad974eccaca
22Pedro22 <pedroaaavvv@gmail.com>
2025-05-10 16:42:40 -0300

    alunos-nutricionista

---
+++ html/clientes-nutricionista.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/clientes-nutricionista.css">
+</head>
+<body>
+    <div class="container">
+        <section class="left">
+            <div class="profile">
+                <figure>
+                    <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                </figure>
+                <h2>Francisco do Nascimento</h2>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/inicio-icon.png" alt="">
+                        </figure>
+                        <a href="">Início</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/user-icon.png" alt="">
+                        </figure>
+                        <a href="">Usuário</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/market-icon.png" alt="">
+                        </figure>
+                        <a href="">Catálogo</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/suporte-icon.png" alt="">
+                        </figure>
+                        <a href="">Suporte</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/config-icon.png" alt="">
+                        </figure>
+                        <a href="">Configurações</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="right">
+            <div class="upper">
+                <h1>Alunos</h1>
+                <input type="search" class="search" name="pesquisar-alunos" id="pesquisasr-alunos" placeholder="Buscar aluno">
+            </div>
+            <div class="clientes">
+                <div class="cliente">
+                    <figure>
+                        <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                    </figure>
+                    <div class="info">
+                        <h3>Francisco do Nascimento</h3>
+                        <p>Busco por evolução constante na musculaão e melhoria do desempenho fisíco</p>
+                        <div class="botao">
+                            <a href="">Ver Detalhes</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="cliente">
+                    <figure>
+                        <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                    </figure>
+                    <div class="info">
+                        <h3>Francisco do Nascimento</h3>
+                        <p>Busco por evolução constante na musculaão e melhoria do desempenho fisíco</p>
+                        <div class="botao">
+                            <a href="">Ver Detalhes</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="cliente">
+                    <figure>
+                        <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                    </figure>
+                    <div class="info">
+                        <h3>Francisco do Nascimento</h3>
+                        <p>Busco por evolução constante na musculaão e melhoria do desempenho fisíco</p>
+                        <div class="botao">
+                            <a href="">Ver Detalhes</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="cliente">
+                    <figure>
+                        <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                    </figure>
+                    <div class="info">
+                        <h3>Francisco do Nascimento</h3>
+                        <p>Busco por evolução constante na musculaão e melhoria do desempenho fisíco</p>
+                        <div class="botao">
+                            <a href="">Ver Detalhes</a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+    </div>
+</body>
+</html>
