93c9d28d490e8df1a448a6de9c86bb48486ab3dc
22Pedro22 <pedroaaavvv@gmail.com>
2025-05-11 15:52:49 -0300

    clientes agendados (tela)

---
+++ html/consultas-agendadas.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/consultas-agendadas.css">
+</head>
+<body>
+    <div class="container">
+        <section class="left">
+            <div class="profile">
+                <figure>
+                    <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                </figure>
+                <h2>Francisco do Nascimento</h2>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/inicio-icon.png" alt="">
+                        </figure>
+                        <a href="">Início</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/user-icon.png" alt="">
+                        </figure>
+                        <a href="">Usuário</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/market-icon.png" alt="">
+                        </figure>
+                        <a href="">Catálogo</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/suporte-icon.png" alt="">
+                        </figure>
+                        <a href="">Suporte</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/config-icon.png" alt="">
+                        </figure>
+                        <a href="">Configurações</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="right">
+            <main>
+                <div class="titulo">
+                    <h1>AGENDADOS</h1>
+                </div>
+                <div class="profissionais">
+                    <div class="nutricionista">
+                        <div class="chat">
+                            <h3>Nutricionista</h3>
+                            <a href="">
+                                <figure>
+                                    <img src="../assets/icons/consultas-agendadas/chat-icon.svg" alt="">
+                                </figure>
+                            </a>
+                        </div>
+                        <div class="info">
+                            <p>Avaliação Antropométrica</p>
+                            <p>Plano Alimentar Personalizado</p>
+                        </div>
+                    </div>
+                    <div class="personal">
+                        <div class="chat">
+                            <h3>Personal</h3>
+                            <a href="">
+                                <figure>
+                                    <img src="../assets/icons/consultas-agendadas/chat-icon.svg" alt="">
+                                </figure>
+                            </a>
+                        </div>
+                        <div class="info">
+                            <p>Definição de plano de treino</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </section>
+    </div>
+</body>
+</html>
