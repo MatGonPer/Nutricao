c8a04424a90439dcf460aa5fb731e4594d813193
22Pedro22 <pedroaaavvv@gmail.com>
2025-05-11 16:06:05 -0300

    marcar consulta (parte 1)

---
+++ html/marcar-consulta.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/consultas-agendadas.css">
+</head>
+<body>
+    <div class="container">
+        <section class="left">
+            <div class="profile">
+                <figure>
+                    <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                </figure>
+                <h2>Francisco do Nascimento</h2>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/inicio-icon.png" alt="">
+                        </figure>
+                        <a href="">Início</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/user-icon.png" alt="">
+                        </figure>
+                        <a href="">Usuário</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/market-icon.png" alt="">
+                        </figure>
+                        <a href="">Catálogo</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/suporte-icon.png" alt="">
+                        </figure>
+                        <a href="">Suporte</a>
+                    </li>
+                    <li class="item">
+                        <figure>
+                            <img src="../assets/icons/user-page/config-icon.png" alt="">
+                        </figure>
+                        <a href="">Configurações</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="right">
+           <main class="marcar-consultas">
+                <section class="profissionais">
+                    <div class="profissional">
+                        <figure>
+                            <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                        </figure>
+                        <h3>Personal Trainer</h3>
+                        <div class="info">
+                            <h4>Francisco do Nascimento</h4>
+                            <p>Emagrecimento e hipertrofia</p>
+                        </div>
+                        <div class="sobre">
+                            <p>Treinador voltado para resultados reais: emagrecimento saudável e ganho de massa muscular. Programas personalizados para transformar o corpo com eficiência.</p>
+                        </div>
+                    </div>
+                    <div class="profissional">
+                        <figure>
+                            <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                        </figure>
+                        <h3>Personal Trainer</h3>
+                        <div class="info">
+                            <h4>Francisco do Nascimento</h4>
+                            <p>Emagrecimento e hipertrofia</p>
+                        </div>
+                        <div class="sobre">
+                            <p>Treinador voltado para resultados reais: emagrecimento saudável e ganho de massa muscular. Programas personalizados para transformar o corpo com eficiência.</p>
+                        </div>
+                    </div>
+                </section>
+           </main>
+        </section>
+    </div>
+</body>
+</html>
