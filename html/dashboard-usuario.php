f35ab4128eef79821194270bf3a3bdbdc446d51a
Arthur <75622979+800Arthur008@users.noreply.github.com>
2025-05-11 09:48:08 -0300

    teste

--- html/dashboard-usuario.php
+++ html/dashboard-usuario.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desempenho do Usuário</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href= "/css/dashboard-usuario.css">
 </head>
 <body>
    <div class="container">
@@ -12,21 +13,23 @@
             <aside>
                 <nav>
                     <ul>
-                        <li><a href="#">Início</a></li>
-                        <li><a href="#">Usuário</a></li>
-                        <li><a href="#">Desempenho</a></li>
-                        <li><a href="#">Treino</a></li>
-                        <li><a href="#">Consultas</a></li>
-                        <li><a href="#">Catálogo</a></li>
-                        <li><a href="#">Suporte</a></li>
-                        <li><a href="#">Configurações</a></li>
-                        <li><a href="#">Sair</a></li>
+                        <li><a  href="#"> <img src="../assets/icons/dashboard-usuario/inicio-icon.svg" alt=""> Início</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/user-icon.svg" alt=""> Usuário</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/desempenho-icon.svg" alt=""> Desempenho</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/treino-icon.svg" alt=""> Treino</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/consulta-icon.svg" alt=""> Consultas</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/market-icon.svg" alt=""> Catálogo</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/suport-icon.svg" alt=""> Suporte</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/config-icon.svg" alt=""> Configurações</a></li>
                     </ul>
                 </nav>
             </aside>
         </section>
-        <section class="right"> 
-
-        </section>
+        <main>
+            <div class="background-main"></div>
+            <section class="right"> 
+    
+            </section>
+        </main>
 </body>
-</html>
+</html
