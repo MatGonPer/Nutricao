1f4ccda48341adadf5148055059deb5e7db437bc
22Pedro22 <pedroaaavvv@gmail.com>
2025-05-12 12:17:07 -0300

    pagina de treino

---
+++ html/treinos.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desempenho do Usuário</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href= "../css/treinos.css">
+</head>
+<body>
+   <div class="container">
+        <section class="left">
+            <div class="profile">
+                <figure>
+                    <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                </figure>
+                <h2>Francisco do Nascimento</h2>
+            </div>
+
+            <aside>
+                <nav>
+                    <ul>
+                        <li><a  href="#"> <img src="../assets/icons/dashboard-usuario/inicio-icon.svg" alt=""> Início</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/user-icon.svg" alt=""> Usuário</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/desempenho-icon.svg" alt=""> Desempenho</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/treino-icon.svg" alt=""> Treino</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/consulta-icon.svg" alt=""> Consultas</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/market-icon.svg" alt=""> Catálogo</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/suport-icon.svg" alt=""> Suporte</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/config-icon.svg" alt=""> Configurações</a></li>
+                    </ul>
+                </nav>
+            </aside>
+        </section>
+        <section class="right">
+            <main>
+                <h1>TREINOS</h1>
+                <article class="treinos">
+                    <div class="treino">
+                        <a href="">
+                            <figure>
+                                <img src="../assets/treinos/supino-reto.png" alt="">
+                            </figure>
+                        </a>
+                        <div class="info">
+                            <p>-</p>
+                            <h2>Supino reto</h2>
+                            <h3>(Barra ou alteres) - 4x12</h3>
+                            <p>Descanso: 2,5 minutos</p>
+                        </div>
+                    </div>
+                    <div class="treino">
+                        <a href="">
+                            <figure>
+                                <img src="../assets/treinos/supino-reto.png" alt="">
+                            </figure>
+                        </a>
+                        <div class="info">
+                            <p>-</p>
+                            <h2>Supino reto</h2>
+                            <h3>(Barra ou alteres) - 4x12</h3>
+                            <p>Descanso: 2,5 minutos</p>
+                        </div>
+                    </div>
+                    <div class="treino">
+                        <a href="">
+                            <figure>
+                                <img src="../assets/treinos/supino-reto.png" alt="">
+                            </figure>
+                        </a>
+                        <div class="info">
+                            <p>-</p>
+                            <h2>Supino reto</h2>
+                            <h3>(Barra ou alteres) - 4x12</h3>
+                            <p>Descanso: 2,5 minutos</p>
+                        </div>
+                    </div>
+                    <div class="treino">
+                        <a href="">
+                            <figure>
+                                <img src="../assets/treinos/supino-reto.png" alt="">
+                            </figure>
+                        </a>
+                        <div class="info">
+                            <p>-</p>
+                            <h2>Supino reto</h2>
+                            <h3>(Barra ou alteres) - 4x12</h3>
+                            <p>Descanso: 2,5 minutos</p>
+                        </div>
+                    </div>
+                    <div class="treino">
+                        <a href="">
+                            <figure>
+                                <img src="../assets/treinos/supino-reto.png" alt="">
+                            </figure>
+                        </a>
+                        <div class="info">
+                            <p>-</p>
+                            <h2>Supino reto</h2>
+                            <h3>(Barra ou alteres) - 4x12</h3>
+                            <p>Descanso: 2,5 minutos</p>
+                        </div>
+                    </div>
+                    <div class="treino">
+                        <a href="">
+                            <figure>
+                                <img src="../assets/treinos/supino-reto.png" alt="">
+                            </figure>
+                        </a>
+                        <div class="info">
+                            <p>-</p>
+                            <h2>Supino reto</h2>
+                            <h3>(Barra ou alteres) - 4x12</h3>
+                            <p>Descanso: 2,5 minutos</p>
+                        </div>
+                    </div>
+                </article>
+            </main>
+        </section>
+    </div>
