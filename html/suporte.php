cb66b8c2dcc8e43adb0f6bccdd89d207756038f4
22Pedro22 <pedroaaavvv@gmail.com>
2025-05-12 15:19:32 -0300

    tela de suporte

---
+++ html/suporte.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desempenho do Usuário</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href= "../css/suporte.css">
+</head>
+<body>
+   <div class="container">
+        <section class="left">
+            <div class="profile">
+                <figure>
+                    <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                </figure>
+                <h2>Francisco do Nascimento</h2>
+            </div>
+
+            <aside>
+                <nav>
+                    <ul>
+                        <li><a  href="#"> <img src="../assets/icons/dashboard-usuario/inicio-icon.svg" alt=""> Início</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/user-icon.svg" alt=""> Usuário</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/desempenho-icon.svg" alt=""> Desempenho</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/treino-icon.svg" alt=""> Treino</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/consulta-icon.svg" alt=""> Consultas</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/market-icon.svg" alt=""> Catálogo</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/suport-icon.svg" alt=""> Suporte</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/config-icon.svg" alt=""> Configurações</a></li>
+                    </ul>
+                </nav>
+            </aside>
+        </section>
+        <section class="right">
+            <div class="contato">
+                <h1>SUPORTE</h1>
+                <div class="email">
+                    <a href="">
+                        <h2>Fale conosco:</h2>
+                        <p>nutrifit@gmail.com</p>
+                    </a>
+                </div>
+            </div>
+            <div class="principais-perguntas">
+                <h3>Principais Dúvidas</h3>
+                <div class="duvidas">
+                    <a href="">
+                        <div class="pergunta">
+                            <p>É normal o sistema demorar para carregar?</p>
+                            <p>></p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="pergunta">
+                            <p>Como posso redefinir minha senha?</p>
+                            <p>></p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="pergunta">
+                            <p>Existe um manual ou guia de uso disponível?</p>
+                            <p>></p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="pergunta">
+                            <p>O sistema possui integração com outras ferramentas?</p>
+                            <p>></p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="pergunta">
+                            <p>Como o sistema protege os meus dados pessoais?</p>
+                            <p>></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
