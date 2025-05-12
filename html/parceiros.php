91f6afaafec99a8dceb503cb7c08749fbac5abfa
22Pedro22 <pedroaaavvv@gmail.com>
2025-05-12 19:50:17 -0300

    ajustes

---
+++ html/parceiros.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marque sua consulta</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/marcar-consulta.css">
+</head>
+<body>
+    <div class="container">
+        <section class="left">
+            <div class="profile">
+                <figure>
+                    <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                </figure>
+                <h2>Francisco do Nascimento</h2>
+            </div>
+            <aside>
+                <nav>
+                    <ul>
+                        <li><a  href="#"> <img src="../assets/icons/dashboard-usuario/inicio-icon.svg" alt=""> Início</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/user-icon.svg" alt=""> Usuário</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/desempenho-icon.svg" alt=""> Desempenho</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/treino-icon.svg" alt=""> Treino</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/consulta-icon.svg" alt=""> Consultas</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/market-icon.svg" alt=""> Catálogo</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/suport-icon.svg" alt=""> Suporte</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/config-icon.svg" alt=""> Configurações</a></li>
+                    </ul>
+                </nav>
+            </aside>
+        </section>
+        <section class="right">
+           <header>
+                <h1>Parceiros</h1>
+           </header>
+           <section class="parceiros">
+                <div class="parceiro">
+                    <figure>
+                        <img src="" alt="">
+                    </figure>
+                    <h2>Growth Supplements</h2>
+                    <p>Nutrição esportiva</p>
+                    <button>Ver</button>
+                </div>
+           </section>
+        </section>
+    </div>
+</body>
+</html>
