c795214c0fb93e6aa3507356b4748338ac6a2b9d
Matheus Gonçalves <matheushtht@gmail.com>
2025-05-16 12:22:31 -0300

    Adicionado nova funcionalidade atualizar() para a classe BancoDeDados

--- model/BancoDeDados.php
+++ model/BancoDeDados.php
@@ -12,7 +12,7 @@ class BancoDeDados {
     private $banco = 'nutrifit';
     private $conexao;
 
-    //Usem essa função para realizar a conexão com o banco
+    //Usem essa função para realizar a conexão com o banco de dados
     public function conectar() : bool {
         $this->conexao = pg_connect("host={$this->servidor} port={$this->porta} dbname={$this->banco} user={$this->usuario} password={$this->senha}");
         if (!$this->conexao) {
@@ -22,14 +22,15 @@ class BancoDeDados {
             return true;
         }   
     }
-    //Usem essa função para fechar a conexão, se não estiver mais precisando do banco feche para economizar recursos
+    //Usem essa função para fechar a conexão, quando você usar o banco e conectar(), lembre de fechar a conexão
+    //depois de utilizar o banco no seu código, TEM QUE FECHAR A CONEXÃO para economizar recursos
     public function desconectar() {
         if($this->conexao) {
             pg_close($this->conexao);
         }
     }
 
-    //Função para inserirDados em uma tabela, array $dados tem que ser do tipo chave e valor
+    //Função para inserir em uma $tabela do banco de dados, $dados tem que ser do tipo CHAVE/VALOR
     //chave será as colunas, e o valor será valor inserido no banco
     public function inserir(string $tabela, array $dados) : bool {
         $colunas = array_keys($dados);
@@ -53,8 +54,8 @@ class BancoDeDados {
         
     }
     
+    //Função para fazer uma consulta de $dados numa $tabela, array de dados tem que ser do tipo CHAVE/VALOR
     public function consultar(string $tabela, array $dados) : array {
-        
         $sql = "SELECT * FROM {$tabela}";
         $valores = [];
 
@@ -82,6 +83,55 @@ class BancoDeDados {
         pg_free_result($resultado);
 
         return $dados ?: [];
+    }
+
+    //Função para atualizar $dados de uma $tabela, array com os $novosDados que serão inserido tem de ser CHAVE/VALOR
+    public function atualizar(string $tabela, array $novosDados, $id) : bool {
+        try {
+            //Separa as colunas de $novosDados, exp: 'nome' e insere no array $colunas
+            $colunas = array_keys($novosDados);
+            //Separa os valores de $novosDados, exp: 'Jorge' e insere no array $valores
+            $valores = array_values($novosDados);
+            //Armazena várias strings que serão usada na consulta, exp: 'nome = $1', 'idade = $2';
+            $atualizacoes = [];
+            $total = count($colunas);
+
+            //Constroi as string para o array $atualizacoes
+            for($i = 0; $i < $total; $i++) {
+                $posicao = $i + 1;
+                $atualizacoes[] = "{$colunas[$i]} = \${$posicao}";
+            }
+
+            //Guarda o id na ultima posicao do array $valores
+            $valores[] = $id;
+            //Guarda a última posicao parametrizada, exp: se for duas colunas, nome = $1 e idade = $2, o ultimo parametro sera $3 posicao para WHERE id = $3
+            $posicaoId = $total + 1;
+
+            //Comando sql
+            $sql = "UPDATE {$tabela} SET " . implode(", ", $atualizacoes) . " WHERE id = \${$posicaoId}";
+        
+            error_log("SQL: " . $sql);
+            error_log("Valores: " . print_r($valores, true));
+
+            $resultado = pg_query_params($this->conexao, $sql, $valores);
+        
+            //Consulta deu errado
+            if($resultado === false) {
+                error_log('Erro ao atualizar: ' . pg_last_error($this->conexao));
+                return false;
+            }
+            
+            //Libera o recurso após pg_query_params()
+            pg_free_result($resultado);
+            return true;
+        } catch(Exception $e) {
+            error_log('Erro na atualização: ' . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function deletar() {
+
     }
 
     public function getConexao() {
