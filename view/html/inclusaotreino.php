fb15705b4a6d299c56b547bd34977cd2f3cab969
LinoLindev <lucianodev.mendes@gmail.com>
2025-05-31 11:58:13 -0300

    tela finalizada no  dia 31/05

---
+++ view/html/inclusaotreino.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/inclusaotreino.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <section class="left">
+            <div class="profile">
+                <figure>
+                    <img src="../assets/perfil-usuario/user-icon.jpg" alt="">
+                </figure>
+                <h2>Francisco do Nascimento</h2>
+            </div>
+            <aside>
+                <nav>
+                    <ul>
+                        <li><a  href="landing-page.php"> <img src="../assets/icons/dashboard-usuario/inicio-icon.svg" alt=""> Início</a></li>
+                        <li><a href="perfil-usuario.php"> <img src="../assets/icons/dashboard-usuario/user-icon.svg" alt=""> Usuário</a></li>
+                        <li><a href="dashboard-usuario.php"> <img src="../assets/icons/dashboard-usuario/desempenho-icon.svg" alt=""> Desempenho</a></li>
+                        <li><a href="treinos.php"> <img src="../assets/icons/dashboard-usuario/treino-icon.svg" alt=""> Treino</a></li>
+                        <li><a href="consultas-agendadas.php"> <img src="../assets/icons/dashboard-usuario/consulta-icon.svg" alt=""> Consultas</a></li>
+                        <li><a href="parceiros.php"> <img src="../assets/icons/dashboard-usuario/market-icon.svg" alt=""> Catálogo</a></li>
+                        <li><a href="suporte.php"> <img src="../assets/icons/dashboard-usuario/suport-icon.svg" alt=""> Suporte</a></li>
+                        <li><a href="#"> <img src="../assets/icons/dashboard-usuario/config-icon.svg" alt=""> Configurações</a></li>
+                    </ul>
+                </nav>
+            </aside>
+        </section>
+        <section class="container-right">
+            <div class="main-card">
+                <div class="top-card">
+                    <div class="alunos-busca">
+                    <h3>ALUNOS</h3>
+                    </div>
+
+                    <div class="search-container">
+                    <input type="text" class="search-box" placeholder="Buscar a lunon">
+                    <button class="search-button"><svg width="10" height="14" viewBox="0 0 18 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M18.5168 16.3918C18.0094 15.9058 17.5156 15.4059 17.0359 14.8925C16.6329 14.483 16.3902 14.1851 16.3902 14.1851L13.3569 12.7367C14.5713 11.3594 15.2415 9.58632 15.2419 7.75008C15.2419 3.5695 11.8402 0.166748 7.65853 0.166748C3.47686 0.166748 0.0751953 3.5695 0.0751953 7.75008C0.0751953 11.9307 3.47686 15.3334 7.65853 15.3334C9.56845 15.3334 11.3094 14.6184 12.6451 13.4495L14.0935 16.4828C14.0935 16.4828 14.3914 16.7255 14.8009 17.1285C15.2202 17.5218 15.7716 18.0537 16.3003 18.6094L17.7714 20.1174L18.4258 20.8173L20.7235 18.5195L20.0237 17.8652C19.6131 17.4622 19.0649 16.927 18.5168 16.3918ZM7.65853 13.1667C4.67178 13.1667 2.24186 10.7368 2.24186 7.75008C2.24186 4.76333 4.67178 2.33342 7.65853 2.33342C10.6453 2.33342 13.0752 4.76333 13.0752 7.75008C13.0752 10.7368 10.6453 13.1667 7.65853 13.1667Z" fill="black"/>
+                    </svg>
+                    </button>
+                    </div>
+
+                </div>
+                <section class="cards">
+                    <div class="card-aluno">
+                        <div class="card-interno">
+                            <img src="../assets/inclusaotreino/plus.svg.svg">
+                        </div>
+                        <p>Adicionar treino</p>
+                    </div>
+                    <div class="card-aluno">
+                        <div class="card-interno">
+                            <img src="../assets/inclusaotreino/plus.svg.svg">
+                        </div>
+                        <p>Adicionar treino</p>
+                    </div>
+                    <div class="card-aluno">
+                        <div class="card-interno">
+                            <img src="../assets/inclusaotreino/plus.svg.svg">
+                        </div>
+                        <p>Adicionar treino</p>
+                    </div>
+                    <div class="card-aluno">
+                        <div class="card-interno">
+                            <img src="../assets/inclusaotreino/plus.svg.svg">
+                        </div>
+                        <p>Adicionar treino</p>
+                    </div>
+                    <div class="card-aluno">
+                        <div class="card-interno">
+                            <img src="../assets/inclusaotreino/plus.svg.svg">
+                        </div>
+                        <p>Adicionar treino</p>
+                    </div>
+                    <div class="card-aluno">
+                        <div class="card-interno">
+                            <img src="../assets/inclusaotreino/plus.svg.svg">
+                        </div>
+                        <p>Adicionar treino</p>
+                    </div>
+                    <div class="card-aluno">
+                        <div class="card-interno">
+                            <img src="../assets/inclusaotreino/plus.svg.svg">
+                        </div>
+                        <p>Adicionar treino</p>
+                    </div>
+
+                </section>
+
+            </div>
+        </section>
+       
+    
+</body>
+</html>
