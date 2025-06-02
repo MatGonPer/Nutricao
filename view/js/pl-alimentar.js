98520adbbfebb49a1b540ef97e1fa204b7fc33a6
Nathanael Silva <153527533+NathanaelSilva@users.noreply.github.com>
2025-06-01 23:05:38 -0300

    Adição de tela e ajustes
    Adição da tela "pl-alimentar-usuario" e ajustes na tela de "pl-alimentar" que seriam as telas de criação de um plano alimentar na visão do usuário e visualização dessas telas pelo cliente.

--- view/js/pl-alimentar.js
+++ view/js/pl-alimentar.js
@@ -7,5 +7,16 @@ document.getElementById('add-refeicao').addEventListener('click', () => {
     <input type="time" name="horario[]" required />
     <textarea name="descricao[]" placeholder="Descrição dos alimentos" required></textarea>
   `;
+
+  const nutrientes = document.createElement("div");
+  nutrientes.classList.add("nutrientes-edicao")
+  nutrientes.innerHTML = `
+    <input type="number"  name="quantidade[]"placeholder="Quantidade total (g)" />
+    <input type="number"  name="proteinas[]" placeholder="Proteínas (g)" />
+    <input type="number"  name="carboidratos[]" placeholder="Carboidratos (g)" />
+    <input type="number"  name="gorduras[]" placeholder="Gorduras (g)" />
+    <input type="number"  name="calorias[]" placeholder="Calorias (kcal)" />
+  `;
+  novaRefeicao.appendChild(nutrientes);
   container.appendChild(novaRefeicao);
 });
